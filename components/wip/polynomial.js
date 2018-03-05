45c6462b59071a8c2dca71f834d138f2a33d9230
Dan Burzo <danburzo@gmail.com>
2018-03-05 20:01:53 +0200

    Natural spline curve

--- components/wip/polynomial.js
+++ components/wip/polynomial.js
@@ -4,6 +4,8 @@ import { scaleLinear, scalePoint, scaleQuantize } from 'd3-scale';
 import React from 'react';
 import { polynomial } from 'everpolate';
 import cubicspline from 'cubic-spline';
+import mathjs from 'mathjs';
+import curveNatural2 from './natural';
 
 const poly = coefficients =>  x => coefficients.reduce((sum, coef, idx, arr) => sum + coef * Math.pow(x, arr.length - 1 - idx), 0);
 
@@ -12,10 +14,10 @@ class Polynomial extends React.Component {
 
 		let controls = [
 			[0, 0],
-			[24, 199],
-			[128, 128],
-			[192, 192],
-			[256, 256]
+			[40, 100],
+			[128, 180],
+			[192, 170],
+			[256, 240]
 		];
 
 		let x_coords = zip(...controls)[0];
@@ -31,12 +33,6 @@ class Polynomial extends React.Component {
 			.range([0, 256]);
 
 		let sample_input = range(0, samp_num).map(i => sample_scale(i));
-
-		// let thresh_scale = scaleQuantize()
-		// 	.domain([0, 256])
-		// 	.range(
-		// 		range(0, controls.length)
-		// 	);
 
 		function polynomial_interpolation_for(value) {
 			let control_idx = controls.indexOf(controls.find(c => c[0] >= value)) - 1;
@@ -59,7 +55,7 @@ class Polynomial extends React.Component {
 		// let sample_output = polynomial(sample_input, zip(...controls)[0], zip(...controls)[1]);
 
 		let xScale = scaleLinear().domain([0, 256]).range([0, w]);
-		let yScale = scaleLinear().domain([0, 256]).range([h, 0]).clamp(true);
+		let yScale = scaleLinear().domain([0, 256]).range([h, 0]);
 
 		let path_func = line()
 			.x(d => xScale(d))
@@ -81,6 +77,18 @@ class Polynomial extends React.Component {
 
 		let basis = basis_func(range(0, controls.length));
 
+		let interpolating_basis_func = line()
+			.x((d, i) => xScale(x_coords[i]))
+			.y((d, i) => yScale(y_coords[i]));
+
+		let interpolating_basis = interpolating_basis_func(range(0, controls.length));
+
+		let interpolating_basis_2 = line()
+			.x((d, i) => xScale(x_coords[i]))
+			.y((d, i) => yScale(y_coords[i]))
+			.curve(curveNatural2)
+			(range(0, controls.length));
+
 
 		let monotonex_func = line()
 			.x((d, i) => xScale(controls[i][0]))
@@ -89,12 +97,13 @@ class Polynomial extends React.Component {
 
 		let monotonex = monotonex_func(range(0, controls.length));
 
-		let monotoney_func = line()
+		let catmull_func = line()
 			.x((d, i) => xScale(controls[i][0]))
 			.y((d, i) => yScale(controls[i][1]))
-			.curve(curveMonotoneY);
+			.curve(curveCatmullRom);
 
-		let monotoney = monotoney_func(range(0, controls.length));
+		let catmull = catmull_func(range(0, controls.length));
+
 
 		return (
 			<svg width={w} height={h} style={{ overflow: 'visible', border: '1px solid #ccc' }}>
@@ -111,19 +120,25 @@ class Polynomial extends React.Component {
 
 				<path d={natural} fill='none' stroke='blue' />
 				<text fill='blue' x='110%' y='20'>natural</text>
+				<text fill='black' x='110%' y='40'>cubic</text>
 
-				<path d={basis} fill='none' stroke='red' />
-				<text fill='red' x='110%' y='40'>basis</text>
+				<g>
+					{/*<path d={basis} fill='none' stroke='red' />
+					<text fill='red' x='110%' y='40'>basis</text>*/}
 
-				<text fill='black' x='110%' y='60'>cubic</text>
+					<path d={interpolating_basis} fill='none' stroke='fuchsia' />
+					<path d={interpolating_basis_2} fill='none' stroke='green' />
 
-				<path d={monotonex} fill='none' stroke='green' />
-				<text fill='green' x='110%' y='80'>monotone x</text>
+{/*
+					<path d={monotonex} fill='none' stroke='green' />
+					<text fill='green' x='110%' y='80'>monotone x</text>
 
-				<path d={monotoney} fill='none' stroke='orange' />
-				<text fill='orange' x='110%' y='100'>monotone y</text>
+					<path d={catmull} fill='none' stroke='orange' />
+					<text fill='orange' x='110%' y='100'>catmull</text>
+*/}
+				</g>
 
-				<path d={path} fill='none' stroke='#000'/>
+				{/*<path d={path} fill='none' stroke='#000'/>*/}
 
 				{ 
 					controls.map(
