0f71f1a7ea38b3c3b0adce68331c3365cb2148f2
Dan Burzo <danburzo@gmail.com>
2018-06-15 16:09:21 +0300

    Replace webpack + babel with rollup + buble; added proptypes

--- components/ColorBand/CheckerboardBand.js
+++ components/ColorBand/CheckerboardBand.js
@@ -1,52 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import guid from '../util/guid';
 
 import './ColorBand.css';
 
 class CheckerboardBand extends React.PureComponent {
-
 	constructor(props) {
 		super(props);
 		this._pattern_id = `checkerboard-pattern-${guid()}`;
 	}
 
 	render() {
-
-		let { 
-			size, 
-			fill 
-		} = this.props;
+		let { size, fill } = this.props;
 
 		return (
-			<svg className='uix-colorband uix-colorband--checkerboard'>
+			<svg className="uix-colorband uix-colorband--checkerboard">
 				<defs>
-					<pattern 
+					<pattern
 						id={this._pattern_id}
-						x='0'
-						y='0'
+						x="0"
+						y="0"
 						width={size}
 						height={size}
 						patternUnits="userSpaceOnUse"
 					>
 						<g fill={fill}>
-							<rect x='0' y='0' width={size} height={size} fill='#fff'/>
-							<rect x='0' y='0' width={size/2} height={size/2}/>
-							<rect x={size/2} y={size/2} width={size/2} height={size/2}/>
+							<rect x="0" y="0" width={size} height={size} fill="#fff" />
+							<rect x="0" y="0" width={size / 2} height={size / 2} />
+							<rect x={size / 2} y={size / 2} width={size / 2} height={size / 2} />
 						</g>
 					</pattern>
 				</defs>
 
-				<rect 
-					x='0' 
-					y='0' 
-					width='100%' 
-					height='100%'
-					fill={`url(#${this._pattern_id})`}
-				/>
+				<rect x="0" y="0" width="100%" height="100%" fill={`url(#${this._pattern_id})`} />
 			</svg>
 		);
 	}
 }
+
+CheckerboardBand.propTypes = {
+	/**
+	 * The size, in pixels, for the checkerboard squares.
+	 */
+	size: PropTypes.number,
+
+	/**
+	 * The fill color for the darker squares.
+	 */
+	fill: PropTypes.string
+};
 
 CheckerboardBand.defaultProps = {
 	size: 10,
--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,28 @@
+import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import buble from 'rollup-plugin-buble';
 import cssbundle from 'rollup-plugin-css-bundle';
 import postcss from 'postcss';
 import autoprefixer from 'autoprefixer';
 import pkg from './package.json';
 
-const css_config = { 
+const css_config = {
 	output: pkg.style,
 	transform: code => postcss([autoprefixer]).process(code, {})
 };
 
 const babel_config = {
 	babelrc: false,
-	presets: ["es2015-rollup", "react"],
-	plugins: ["transform-object-rest-spread"],
+	presets: ['es2015-rollup', 'react'],
+	plugins: ['transform-object-rest-spread'],
 	exclude: ['node_modules/**']
 };
 
-export default [{
-	input: 'components/index.js',
-	external: Object.keys(pkg.dependencies),
-	output: [
-		{ file: pkg.main, format: 'cjs' },
-		{ file: pkg.module, format: 'es' },
-		{
-			file: pkg.browser,
-			format: 'umd',
-			name: 'uiuiui',
-			globals: {
-				'react': 'React',
-				'react-dom': 'ReactDOM',
-				'react-event-listener': 'EventListener'
-			}
-		}
-	],
-	plugins: [
-		resolve(),
-		cssbundle(css_config),
-		babel(babel_config)
-	]
-}];
+export default [
+	{
+		input: 'components/index.js',
+		external: Object.keys(pkg.dependencies),
+		output: [{ file: pkg.main, format: 'cjs' }, { file: pkg.module, format: 'es' }],
+		plugins: [cssbundle(css_config), buble({ objectAssign: true }), resolve(), commonjs()]
+	}
+];
