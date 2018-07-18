b196161e5267f8f9a30c7cd15c2ba73f71ce3b25
Darius Ungurean <dariusungurean@gmail.com>
2018-07-18 16:49:21 +0300

    Added some missing ColorInput code, upgraded culori and culori-scales, upgraded package version

--- components/Input/ColorInput.js
+++ components/Input/ColorInput.js
@@ -9,11 +9,15 @@ import { identity, noop } from '../util/functions';
 import './ColorInput.css';
 
 class ColorInput extends React.Component {
-
 	static getDerivedStateFromProps(props, current_state) {
-		return props.value !== current_state.value ? { value: props.value } : null;
+		return props.value !== current_state.prev_prop_value
+			? {
+					value: props.value,
+					prev_prop_value: props.value
+			  }
+			: null;
 	}
-	
+
 	constructor(props) {
 		super(props);
 		this.change = this.change.bind(this);
@@ -23,9 +27,10 @@ class ColorInput extends React.Component {
 		this.onKeyDown = this.onKeyDown.bind(this);
 		this.onKeyUp = this.onKeyUp.bind(this);
 
-		this.state = { 
+		this.state = {
 			interacting: false,
-			initial_value: props.value
+			initial_value: props.value,
+			prev_prop_value: undefined
 		};
 	}
 
@@ -40,18 +45,27 @@ class ColorInput extends React.Component {
 	}
 
 	start() {
-		this.setState({ interacting: true }, () => { this.props.onStart() });
+		this.setState({ interacting: true }, () => {
+			this.props.onStart();
+		});
 	}
 
 	end() {
-		this.setState({ interacting: false }, () => { this.props.onEnd() });
+		this.setState({ interacting: false }, () => {
+			this.props.onEnd();
+		});
 	}
 
 	cancel() {
-		this.setState({ 
-			interacting: false,
-			value: this.state.initial_value
-		}, () => { this.props.onCancel() });
+		this.setState(
+			{
+				interacting: false,
+				value: this.state.initial_value
+			},
+			() => {
+				this.props.onCancel();
+			}
+		);
 	}
 
 	register(el) {
@@ -79,62 +93,44 @@ class ColorInput extends React.Component {
 	}
 
 	render() {
+		let { interacting, value } = this.state;
 
-		let { 
-			interacting,
-			value
-		} = this.state;
-
-		let {
-			className,
-			target,
-			mirror
-		} = this.props;
+		let { className, target, mirror } = this.props;
 
 		return (
 			<div
 				className={`
 					uix-colorinput
-					${ className || '' }
+					${className || ''}
 				`}
 				ref={this.register}
 			>
-				<div 
-					className='uix-colorinput__current'
-					onClick={this.start}
-				>
-					{ this.props.current(value) }
+				<div className="uix-colorinput__current" onClick={this.start}>
+					{this.props.current(value)}
 				</div>
-				{
-					interacting &&
-						<Portal target={ target } mirror={ mirror || this._el }>
-							<Popup 
-								className={`
+				{interacting && (
+					<Portal target={target} mirror={mirror || this._el}>
+						<Popup
+							className={`
 									uix-colorinput__popup 
-									${ target ? 'uix-colorinput__popup--portal' : '' }
-								`} 
-								onClose={this.end}
-							>
-								<EventListener
-									target='document'
-									onKeyDown={ withOptions(this.onKeyDown, { capture: true }) }
-									onKeyUp={ withOptions(this.onKeyUp, { capture: true }) }
-								/>
-								{ 
-									React.Children.map(
-										this.props.children,
-										child => React.cloneElement(
-											child,
-											{
-												value: value,
-												onChange: this.change
-											}
-										)
-									)
-								}
-							</Popup>
-						</Portal>
-				}
+									${target ? 'uix-colorinput__popup--portal' : ''}
+								`}
+							onClose={this.end}
+						>
+							<EventListener
+								target="document"
+								onKeyDown={withOptions(this.onKeyDown, { capture: true })}
+								onKeyUp={withOptions(this.onKeyUp, { capture: true })}
+							/>
+							{React.Children.map(this.props.children, child =>
+								React.cloneElement(child, {
+									value: value,
+									onChange: this.change
+								})
+							)}
+						</Popup>
+					</Portal>
+				)}
 			</div>
 		);
 	}
